52abe94dc95611550951390665e49cee8e3f0285
webkida-ss <a5113067.171452@gmail.com>
2022-08-08 19:18:25 +0900

    追記

--- src/main/java/project/basic/my_object/UserCustom.java
+++ src/main/java/project/basic/my_object/UserCustom.java
@@ -1,24 +1,26 @@
 package project.basic.my_object;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Data//
 public class UserCustom {
     private int id;
     private String name;
     private int age;
 
+    /*
+     * 個別に定義すれば、@Dataよりも優先される
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
         UserCustom userCustom = (UserCustom)obj;
         String userName = userCustom.getName();
         return this.name.equals(userName);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
     }
 }
